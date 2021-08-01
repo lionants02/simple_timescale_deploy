cb205787d87efb479263614a27635b51aed5fb85
Max Thanachai Thongkum <singmax161@gmail.com>
2021-08-01 22:55:16 +0700

    Update db.sh

--- db.sh
+++ db.sh
@@ -10,8 +10,12 @@ docker service rm ${SERVICE_NAME}
 docker service create --replicas 1 \
     --name ${SERVICE_NAME} \
     --publish published=15474,target=5432 \
+    --mount type=bind,src=/home/thanachai/timescale/data,dst=/var/lib/postgresql/data/pgdata \
+    --env "PGDATA=/var/lib/postgresql/data/pgdata"
+    --env "POSTGRES_USER=xxxxxxx" \
+    --env "POSTGRES_PASSWORD=xxxxxxx" \
     --health-cmd='ls' \
-    --health-timeout=10s \
+    --health-timeout=15s \
     --health-retries=3 \
     --health-interval=5s \
     ${IMAGE_TAG}
